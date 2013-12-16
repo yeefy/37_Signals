6d5096dd9e37ea66bc5196a60f64897c00cb1f04
yeefy <yftran@gmail.com>
2013-12-16 13:00:43 -0800

    jQuery
    Attempt 3

--- application.js
+++ application.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('#base').mouseenter(function() {
         $('#headingtxt').hide();
+	});
+	$('#base').mouseleave(function() {
+        $('#headingtxt').show();
     });
 });// JavaScript Document
