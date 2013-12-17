eff17dbc26c10c0fd191551adb9e42c438fef5ff
yeefy <yftran@gmail.com>
2013-12-16 20:40:51 -0800

    jQuery with arrows

--- application.js
+++ application.js
@@ -1,8 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function(e) {
     $('#base').mouseenter(function() {
-        $('#headingtxt').hide();
+        $('.headingtxt').hide();
+		$('.headingtxt_base').show();
+		$('.arrow_left').addClass('arrow_left_on');
 	});
+	
 	$('#base').mouseleave(function() {
-        $('#headingtxt').show();
+		$('.headingtxt_base').hide();
+        $('.headingtxt').show();
+		$('.arrow_left').removeClass('arrow_left_on');
     });
-});// JavaScript Document
+	
+	$('#high').mouseenter(function() {
+        $('.headingtxt').hide();
+		$('.headingtxt_high').show();
+    });
+	$('#high').mouseleave(function() {
+		$('.headingtxt_high').hide();
+		$('.headingtxt').show();
+	});
+	
+	$('#camp').mouseenter(function(){
+		$('.headingtxt').hide();
+		$('.headingtxt_camp').show();
+	});
+	$('#camp').mouseleave(function() {
+        $('.headingtxt_camp').hide();
+		$('.headingtxt').show();
+    });
+});
