037d643dc30a2ccb0e0874d54a447ec11fb82422
yeefy <yftran@gmail.com>
2013-12-20 23:12:24 -0800

    jQuery
    Successfully added left arrow

--- application.js
+++ application.js
@@ -1,31 +1,43 @@
-$(document).ready(function(e) {
+$(document).ready(function() {
     $('#base').mouseenter(function() {
         $('.headingtxt').hide();
+		$('.arrow_left').hide;
 		$('.headingtxt_base').show();
-		$('#arrow_left').addClass('arrow_left_on');
+		$('.arrow_left_on').show();
 	});
 	
 	$('#base').mouseleave(function() {
 		$('.headingtxt_base').hide();
+		$('.arrow_left_on').hide();
         $('.headingtxt').show();
-		$('#arrow_left').removeClass('arrow_left_on');
+		$('.arrow_left').show();
     });
 	
 	$('#high').mouseenter(function() {
         $('.headingtxt').hide();
+		$('.arrow_mid').hide();
 		$('.headingtxt_high').show();
+		$('.arrow_mid_on').show();
     });
+	
 	$('#high').mouseleave(function() {
 		$('.headingtxt_high').hide();
+		$('.arrow_mid_on').hide();
 		$('.headingtxt').show();
+		$('.arrow_mid').show();
 	});
 	
 	$('#camp').mouseenter(function(){
 		$('.headingtxt').hide();
+		$('.arrow_right').hide();
 		$('.headingtxt_camp').show();
+		$('.arrow_right_on').show();
 	});
+	
 	$('#camp').mouseleave(function() {
         $('.headingtxt_camp').hide();
+		$('.arrow_right_on').hide();
 		$('.headingtxt').show();
+		$('.arrow_right').show();
     });
 });
